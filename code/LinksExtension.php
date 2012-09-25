fdb08e07b9688f98453c3453a77d86551384ec71
Gordon Anderson <gordon.b.anderson@gmail.com>
2012-09-25 12:42:50 +0700

    FIX: Removed errorlog statement and reformatted code

--- code/LinksExtension.php
+++ code/LinksExtension.php
@@ -2,33 +2,27 @@
 
 class LinksExtension extends DataObjectDecorator {
 
+  function extraStatics() {
+    return array(
+      'belongs_many_many' => array(
+        'Links' => 'Link',
+      )
+    );
+  }
 
-	
-	function extraStatics() { 
-      return array( 
-         'belongs_many_many' => array( 
-            'Links' => 'Link', 
-         ) 
-      ); 
-   }
+  public function updateCMSFields( FieldSet &$fields ) {
 
-
-   public function updateCMSFields(FieldSet &$fields) {
-
-      if (in_array($this->owner->qClassName, Link::$classesToAddLinksTo)) {
-        $manager = new ManyManyDataObjectManager($this->owner, "Links", "Link", array('Title' => 'Title', 'URL' => 'URL'), "getCMSFields_forPopup");
-        $manager->setPluralTitle('Links');
-        $fields->addFieldToTab("Root.Content.Links", $manager);  
-      }
-      
-   }    
+    if ( in_array( $this->owner->qClassName, Link::$classesToAddLinksTo ) ) {
+      $manager = new ManyManyDataObjectManager( $this->owner, "Links", "Link", array( 'Title' => 'Title', 'URL' => 'URL' ), "getCMSFields_forPopup" );
+      $manager->setPluralTitle( 'Links' );
+      $fields->addFieldToTab( "Root.Content.Links", $manager );
+    }
+  }
 
 
-
-	public function getFooterLinksFolder() {
-		error_log("Getting footer links folder");
-		return DataObject::get_one('FooterLinksFolder');
-	}
+  public function getFooterLinksFolder() {
+    return DataObject::get_one( 'FooterLinksFolder' );
+  }
 
 }
 

974b125b62701a1fae1edea59d2577a3d0b7a4d8
Gordon Anderson <gordon.b.anderson@gmail.com>
2012-09-25 12:42:35 +0700

    FIX: Removed errorlog statement and reformatted code

--- code/Link.php
+++ code/Link.php
@@ -3,58 +3,44 @@
  * Defines the Link page type
  */
 class Link extends DataObject {
-   static $db = array(
-	'URL' => 'Text',
-	'Title' => 'Text',
-	'Description' => 'HTMLText',
-	"LinkType" => "Enum('External,Internal')",
-   );
-
-
-     static $classesToAddLinksTo = array('Page');
-
-
-
-
-
-
-
-    
-    static $has_one = array(
-	   'LinksFolder' => 'LinksFolder',
-      'InternalPage' => 'SiteTree'
+	static $db = array(
+		'URL' => 'Text',
+		'Title' => 'Text',
+		'Description' => 'HTMLText',
+		"LinkType" => "Enum('External,Internal')",
 	);
 
-	public static $many_many = array( 
+
+	static $classesToAddLinksTo = array('Page');
+
+
+	static $has_one = array(
+		'LinksFolder' => 'LinksFolder',
+		'InternalPage' => 'SiteTree'
+	);
+
+	public static $many_many = array(
 		"Pages" => "SiteTree"
 	);
 
 
-
-    
-
-
-
-    function getRequirementsForPopup() {
-    	error_log("Link get requirements for popup");
-    	        Requirements::javascript('silverstripe-links/javascript/linkedit.js');
-
-    }
-  
- 
-
-	function getCMSFields_forPopup () {
+	function getRequirementsForPopup() {
+		Requirements::javascript( 'silverstripe-links/javascript/linkedit.js' );
+	}
 
 
-	   $fields = new FieldSet(
-	   		new TextField('Title'),
-	   		 new DropdownField('LinkType', 'Internal or External Link',
-         	singleton('Link')->dbObject('LinkType')->enumValues()
-         ),
+	function getCMSFields_forPopup() {
 
-	   		new TextField('URL'),
-	   	 	new LiveDropdownField("InternalPageID", "Choose an internal link", "SiteTree"),
-			new SimpleTinyMCEField('Description')
+
+		$fields = new FieldSet(
+			new TextField('Title'),
+			new DropdownField( 'LinkType', 'Internal or External Link',
+				singleton('Link')->dbObject( 'LinkType' )->enumValues()
+			),
+
+			new TextField('URL'),
+			new LiveDropdownField( "InternalPageID", "Choose an internal link", "SiteTree" ),
+			new SimpleTinyMCEField( 'Description' )
 		);
 
 		return $fields;
@@ -62,9 +48,7 @@ class Link extends DataObject {
 
 
 	function LoadLink() {
-		//    return DataObject::get("SiteTree","ParentID = ($this->ParentID)")->Count();
-
-		$refreshedLink = DataObject::get_one("Link", "Link_Live.ID=".$this->ID);
+		$refreshedLink = DataObject::get_one( "Link", "Link_Live.ID=".$this->ID );
 		return $refreshedLink->URL;
 	}
 
@@ -72,16 +56,16 @@ class Link extends DataObject {
 	function getWebsiteAddress() {
 		$result = $this->URL;
 
-		if ($this->LinkType == 'Internal') {
-			$targetPage = DataObject::get_by_id('Page', $this->InternalPageID);
-			if ($targetPage) {
+		if ( $this->LinkType == 'Internal' ) {
+			$targetPage = DataObject::get_by_id( 'Page', $this->InternalPageID );
+			if ( $targetPage ) {
 				$result = $targetPage->Link();
 			} else {
 				$result = '#';
 			}
 		}
 
-		if (!$result) {
+		if ( !$result ) {
 			$result =  '#';
 		}
 		return $result;

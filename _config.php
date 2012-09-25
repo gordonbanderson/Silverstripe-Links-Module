7aee998e1d048aa84ed85d84f18325ba48cc1c89
Gordon Anderson <gordon.b.anderson@gmail.com>
2012-09-25 16:33:32 +0700

    ENHANCEMENT: Automatically enable the module

--- _config.php
+++ _config.php
@@ -1,4 +1,7 @@
 <?php
 // make links sortable
 SortableDataObject::add_sortable_class('Link');
+
+// enabled the extension, need the FooterLinksFolder method to be available
+Object::add_extension('SiteTree', 'LinksExtension');
 ?>

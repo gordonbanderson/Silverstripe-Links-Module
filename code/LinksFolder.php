42fe413a1fa8b22e5b399641b648aafeba1150c8
Gordon Anderson <gordon.b.anderson@gmail.com>
2012-11-22 14:31:53 +0700

    UPGRADESS3: Now compliant with ss3. Added grid field, but still a few issues to sort out

--- code/LinksFolder.php
+++ code/LinksFolder.php
@@ -9,21 +9,36 @@ class LinksFolder extends Page {
 
   static $has_many = array('Links' => 'Link');
 
+/*
+	static $many_many_extraFields = array( 
+        'Links' => array( 
+                'SortOrder' => "Int" 
+        )
+	);
+*/
    function getCMSFields() {
 		$fields = parent::getCMSFields();
 		$fields->renameField("Content", "Description");
 
-			$mng_records = new DataObjectManager(
-			$this,
-			'Links',
-			'Link',
-			array('Title' => 'Title','URL' => 'URL'),
-			'getCMSFields_forPopup'
-		);
+
+		$gridFieldConfig = GridFieldConfig::create()->addComponents(
+          new GridFieldToolbarHeader(),
+          new GridFieldAddNewButton('toolbar-header-right'),
+          new GridFieldSortableHeader(),
+          new GridFieldDataColumns(),
+          new GridFieldPaginator(10),
+          new GridFieldEditButton(),
+          new GridFieldDeleteAction('unlinkRelation'),
+          new GridFieldDetailForm()
+//          new GridFieldBulkEditingTools()
+        );
+
+        $gridField = new GridField("Links", "List of Links:", $this->Links()->sort('SortOrder'), $gridFieldConfig);
+        $fields->addFieldToTab("Root.Links", $gridField);
 
 
 
-		$fields->addFieldToTab('Root.Content.Links', $mng_records);
+		//$fields->addFieldToTab('Root.Content.Links', $mng_records);
 
 	   return $fields;
 	}

901ac35514cf3acb4f54fc19a2dd1a80e83efdf4
Gordon Anderson <gordon.b.anderson@gmail.com>
2012-11-22 14:28:38 +0700

    UPGRADESS3: Remove data object manager sort definition

--- _config.php
+++ _config.php
@@ -1,6 +1,6 @@
 <?php
 // make links sortable
-SortableDataObject::add_sortable_class('Link');
+//SortableDataObject::add_sortable_class('Link');
 
 // enabled the extension, need the FooterLinksFolder method to be available
 Object::add_extension('SiteTree', 'LinksExtension');

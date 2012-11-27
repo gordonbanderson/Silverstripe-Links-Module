cd3a1ae73101d72b4d81d38e9d997588d787eafa
Gordon Anderson <gordon.b.anderson@gmail.com>
2012-11-27 15:38:37 +0700

    BUGFIX: Formatting of code

--- code/LinksFolder.php
+++ code/LinksFolder.php
@@ -3,35 +3,26 @@
  * Defines the Link page type
  */
 class LinksFolder extends Page {
-   static $db = array(
-   );
-  
+  static $db = array(
+  );
 
-  static $has_many = array('Links' => 'Link');
 
-/*
-	static $many_many_extraFields = array( 
-        'Links' => array( 
-                'SortOrder' => "Int" 
-        )
-	);
-*/
-   function getCMSFields() {
-      $fields = parent::getCMSFields();
-      $fields->renameField("Content", "Description");
-      $gridConfig = GridFieldConfig_RelationEditor::create()->addComponent(new GridFieldSortableRows('SortOrder'));
-      $gridConfig->getComponentByType('GridFieldAddExistingAutocompleter')->setSearchFields(array('URL', 'Title','Description'));
-      //$jsInclude = new GridFieldLinkEdit();
-      //$gridConfig->addComponent($jsInclude);
-      $gridField = new GridField("Links", "List of Links:", $this->Links()->sort('SortOrder'), $gridConfig);
-      $fields->addFieldToTab("Root.Links", $gridField);
-	   return $fields;
-	}
+  static $has_many = array( 'Links' => 'Link' );
+
+  function getCMSFields() {
+    $fields = parent::getCMSFields();
+    $fields->renameField( "Content", "Description" );
+    $gridConfig = GridFieldConfig_RelationEditor::create()->addComponent( new GridFieldSortableRows( 'SortOrder' ) );
+    $gridConfig->getComponentByType( 'GridFieldAddExistingAutocompleter' )->setSearchFields( array( 'URL', 'Title', 'Description' ) );
+    $gridField = new GridField( "Links", "List of Links:", $this->Links()->sort( 'SortOrder' ), $gridConfig );
+    $fields->addFieldToTab( "Root.Links", $gridField );
+    return $fields;
+  }
 
 }
- 
+
 class LinksFolder_Controller extends Page_Controller {
- 
+
 }
- 
+
 ?>

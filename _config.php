f55dd38f22e8b2586ef8ec8263c0803d655487a3
Gordon Anderson <gordon.b.anderson@gmail.com>
2012-11-27 15:20:14 +0700

    ENHANCEMENT: Add edit tools path so that no hardcoding is required

--- _config.php
+++ _config.php
@@ -4,4 +4,10 @@
 
 // enabled the extension, need the FooterLinksFolder method to be available
 Object::add_extension('SiteTree', 'LinksExtension');
+
+//define global path to Components' root folder
+if(!defined('LINK_EDIT_TOOLS_PATH'))
+{
+	define('LINK_EDIT_TOOLS_PATH', rtrim(basename(dirname(__FILE__))));
+}
 ?>

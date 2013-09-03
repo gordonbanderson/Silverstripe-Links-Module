d5430ddb720bb117db9732f2209ff52cda818511
Gordon Anderson <gordon.b.anderson@gmail.com>
2013-09-03 10:13:22 +0700

    FIX: Ensure form works all the time using entwine

--- javascript/linkedit.js
+++ javascript/linkedit.js
@@ -1,7 +1,34 @@
 /*jslint white: true */
 (function($) {
 	$(document).ready(function() {
-		// Find the select box, named differently on the update and add forms
+
+		$('#Form_ItemEditForm_LinkType').entwine({
+			onchange: function(e) {
+				var sel = $(e.target);
+				console.log(sel);
+				if(sel.val() == 'Internal') {
+					$('#URL').addClass('hide');
+					$('#InternalPageID').removeClass('hide');
+				} else {
+					$('#URL').removeClass('hide');
+					$('#InternalPageID').addClass('hide');
+				}
+			},
+			onmatch: function(e) {
+				var sel = $(e.target);
+
+				// hide either the internal or external link editing box depending on which link type the link is
+				// (internal or external)
+				if(sel.val() == 'Internal') {
+					$('#URL').addClass('hide');
+				} else {
+					$('#InternalPageID').addClass('hide');
+				}
+			}
+			
+		});
+
+		// this is run first time only, to prime the form
 		var sel = $('#Form_ItemEditForm_LinkType');
 
 		// hide either the internal or external link editing box depending on which link type the link is
@@ -11,16 +38,5 @@
 		} else {
 			$('#InternalPageID').addClass('hide');
 		}
-
-		// toggle boxes on drop down change
-		sel.change(function(e) {
-			if(sel.val() == 'Internal') {
-				$('#URL').addClass('hide');
-				$('#InternalPageID').removeClass('hide');
-			} else {
-				$('#URL').removeClass('hide');
-				$('#InternalPageID').addClass('hide');
-			}
-		});
 	});
 })(jQuery);

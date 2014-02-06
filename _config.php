d4e7e471936cade695c1da31a7f9f3047b0ac8b8
Gordon Anderson <gordon.b.anderson@gmail.com>
2014-02-06 22:46:54 +0700

    MINOR: Removed non 3.1 compatible config

--- _config.php
+++ _config.php
@@ -1,13 +1,7 @@
 <?php
-// make links sortable
-//SortableDataObject::add_sortable_class('Link');
-
-// enabled the extension, need the FooterLinksFolder method to be available
-Object::add_extension('SiteTree', 'LinksExtension');
 
 //define global path to Components' root folder
 if(!defined('LINK_EDIT_TOOLS_PATH'))
 {
 	define('LINK_EDIT_TOOLS_PATH', rtrim(basename(dirname(__FILE__))));
 }
-?>

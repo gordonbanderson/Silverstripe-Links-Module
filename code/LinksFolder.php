41812d4e9c6b6e42bec2d41f5241e832856dc4ea
Gordon Anderson <gordon.b.anderson@gmail.com>
2014-02-06 22:55:22 +0700

    ENHANCEMENT: Use of named page slots

--- code/LinksFolder.php
+++ code/LinksFolder.php
@@ -4,13 +4,19 @@
  */
 class LinksFolder extends Page {
   static $db = array(
+    'SlotName' => 'Varchar(255)'
   );
+
+  private static $icon = 'weboftalent-links/icons/link.png'; 
+
 
 
   static $has_many = array( 'Links' => 'Link' );
 
   function getCMSFields() {
     $fields = parent::getCMSFields();
+    $nameField = new TextField('SlotName', _t('Link.SLOT_NAME', 'Name of the slot on the page where these links will appear'));
+    $fields->addFieldToTab('Root.PageSlot', $nameField);
     $fields->renameField( "Content", "Description" );
     $gridConfig = GridFieldConfig_RelationEditor::create()->addComponent( new GridFieldSortableRows( 'SortOrder' ) );
     $gridConfig->getComponentByType( 'GridFieldAddExistingAutocompleter' )->setSearchFields( array( 'URL', 'Title', 'Description' ) );

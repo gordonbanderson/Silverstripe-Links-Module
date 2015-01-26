a6ec4ceac10c132ff257043a5508eb4b5fffca9d
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-01-26 11:40:38 +0700

    FIX: Ensure internal page selection field is correctly hidden, it requires two points where the hide class needs to be removed

--- javascript/linkedit.js
+++ javascript/linkedit.js
@@ -5,23 +5,13 @@
 		$('#Form_ItemEditForm_LinkType').entwine({
 			onchange: function(e) {
 				var sel = $(e.target);
-				console.log(sel);
-				if(sel.val() == 'Internal') {
+				if(sel.val() === 'Internal') {
 					$('#URL').addClass('hide');
 					$('#InternalPageID').removeClass('hide');
+					// the hide class is added twice by addExtraClass
+					$('#TreeDropdownField_Form_ItemEditForm_InternalPageID').removeClass('hide');
 				} else {
 					$('#URL').removeClass('hide');
-					$('#InternalPageID').addClass('hide');
-				}
-			},
-			onmatch: function(e) {
-				var sel = $(e.target);
-
-				// hide either the internal or external link editing box depending on which link type the link is
-				// (internal or external)
-				if(sel.val() == 'Internal') {
-					$('#URL').addClass('hide');
-				} else {
 					$('#InternalPageID').addClass('hide');
 				}
 			}

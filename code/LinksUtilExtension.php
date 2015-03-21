5b98482b1c005ed4837bf7d3f391bec8f958f64f
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-03-21 15:27:33 +0700

    FIX: Whitespace

--- code/LinksUtilExtension.php
+++ code/LinksUtilExtension.php
@@ -5,13 +5,12 @@ Methods require to access links from any page on the site - this needs added to 
 */
 class LinksUtilExtension extends DataExtension {
 
-  public function getFooterLinksFolder() {
-    return DataObject::get_one( 'FooterLinksFolder' );
-  }
+	public function getFooterLinksFolder() {
+		return DataObject::get_one( 'FooterLinksFolder' );
+	}
 
-  public function LinksFolderForSlot($slotname) {
-    error_log('+++++++++++++++++++++++++++++++++++++ slot for links folder '+$slotname);
-    return LinksFolder::get()->filter('SlotName',$slotname) -> first();
-  }
+	public function LinksFolderForSlot($slotname) {
+		return LinksFolder::get()->filter('SlotName',$slotname) -> first();
+	}
 
 }

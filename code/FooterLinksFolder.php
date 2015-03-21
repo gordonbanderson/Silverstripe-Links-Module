415585008dd7fdd6d0cb15e274fbbde2cdfd96f7
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-03-21 15:26:49 +0700

    FIX: Whitespace

--- code/FooterLinksFolder.php
+++ code/FooterLinksFolder.php
@@ -3,25 +3,19 @@
  * Defines the Link page type
  */
 class FooterLinksFolder extends Page {
-	static $db = array(
-	);
 
-  	// a footer links folder shall contain links folders - these will be rendered as columns in the footer
+	// A footer links folder shall contain links folders -
+	// these will be rendered as columns in the footer
 	static $allowed_children = array('LinksFolder');
 
-	static $defaults = array( 
-    	'ShowInMenus' => 0,
-    	'ShowInSearch' => 0
-    );
+	static $defaults = array(
+		'ShowInMenus' => 0,
+		'ShowInSearch' => 0
+	);
 
-    private static $icon = 'weboftalent-links/icons/link.png'; 
-
-
-
+	private static $icon = 'weboftalent-links/icons/link.png';
 }
- 
+
 class FooterLinksFolder_Controller extends Page_Controller {
- 
+
 }
- 
-?>

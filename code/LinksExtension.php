08eef75df4f8b1aee232d71fcfd7212e6282814e
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-03-21 15:27:07 +0700

    FIX: Whitespace

--- code/LinksExtension.php
+++ code/LinksExtension.php
@@ -2,28 +2,28 @@
 
 class LinksExtension extends DataExtension {
 
-  static $belongs_many_many = array( 'Links' => 'Link' );
+	static $belongs_many_many = array( 'Links' => 'Link' );
 
-  static $belongs_many_many_extraFields = array(
-    'Links' => array(
-      'SortOrder' => "Int"
-    )
-  );
+	static $belongs_many_many_extraFields = array(
+		'Links' => array(
+			'SortOrder' => "Int"
+		)
+	);
 
 
-  /* Edit links */
-  public function updateCMSFields( FieldList $fields ) {
-        $gridFieldConfig = GridFieldConfig::create()->addComponents(
-          new GridFieldToolbarHeader(),
-          new GridFieldAddNewButton('toolbar-header-right'),
-          new GridFieldSortableHeader(),
-          new GridFieldDataColumns(),
-          new GridFieldPaginator(10),
-          new GridFieldEditButton(),
-          new GridFieldDeleteAction(),
-          new GridFieldDetailForm()
-        );
-        $gridField = new GridField("Links", "List of Links:", $this->owner->Links(), $gridFieldConfig);
-        $fields->addFieldToTab("Root.Links", $gridField);
-    }
+	/* Edit links */
+	public function updateCMSFields( FieldList $fields ) {
+		$gridFieldConfig = GridFieldConfig::create()->addComponents(
+			new GridFieldToolbarHeader(),
+			new GridFieldAddNewButton('toolbar-header-right'),
+			new GridFieldSortableHeader(),
+			new GridFieldDataColumns(),
+			new GridFieldPaginator(10),
+			new GridFieldEditButton(),
+			new GridFieldDeleteAction(),
+			new GridFieldDetailForm()
+		);
+		$gridField = new GridField("Links", "List of Links:", $this->owner->Links(), $gridFieldConfig);
+		$fields->addFieldToTab("Root.Links", $gridField);
+	}
 }

53f18fbd435ec1f2b965bdd78a95f2440ee74a5d
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-02-27 15:46:38 +0700

    FIX: Use entwine so that a new link has the correct settigns in the GUI

--- javascript/linkedit.js
+++ javascript/linkedit.js
@@ -1,26 +1,24 @@
 /*jslint white: true */
 (function($) {
-	$(document).ready(function() {
-		// Find the select box, named differently on the update and add forms
-		var sel = $('#Form_ItemEditForm_LinkType');
-
-		// hide either the internal or external link editing box depending on which link type the link is
-		// (internal or external)
-		if(sel.val() == 'Internal') {
-			$('#Form_ItemEditForm_URL_Holder').hide();
-		} else {
-			$('#Form_ItemEditForm_InternalPageID_Holder').hide();
-		}
-
-		// toggle boxes on drop down change
-		sel.change(function(e) {
-			if(sel.val() == 'Internal') {
-				$('#Form_ItemEditForm_URL_Holder').hide();
-				$('#Form_ItemEditForm_InternalPageID_Holder').show();
-			} else {
-				$('#Form_ItemEditForm_URL_Holder').show();
-				$('#Form_ItemEditForm_InternalPageID_Holder').hide();
-			}
-		});
-	});
+    $.entwine(function($) {
+        $('#Form_ItemEditForm_LinkType').entwine({
+            onmatch: function(e) {
+                if(this.val() == 'Internal') {
+					$('#Form_ItemEditForm_URL_Holder').hide();
+				} else {
+					$('#Form_ItemEditForm_InternalPageID_Holder').hide();
+				}
+                this._super();
+            },
+            onchange: function(e) {
+            	if(this.val() == 'Internal') {
+					$('#Form_ItemEditForm_URL_Holder').hide();
+					$('#Form_ItemEditForm_InternalPageID_Holder').show();
+				} else {
+					$('#Form_ItemEditForm_URL_Holder').show();
+					$('#Form_ItemEditForm_InternalPageID_Holder').hide();
+				}
+            }
+        });
+    });
 })(jQuery);

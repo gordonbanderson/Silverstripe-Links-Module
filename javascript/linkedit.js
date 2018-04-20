a905f7db9f2e74e0db8dea17302d3a35b6c63d01
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-04-20 14:00:17 +0700

    FIX: Remove entwine references

--- javascript/linkedit.js
+++ javascript/linkedit.js
@@ -1,24 +1,24 @@
 /*jslint white: true */
 (function($) {
-    $.entwine(function($) {
-        $('#Form_ItemEditForm_LinkType').entwine({
-            onmatch: function(e) {
-                if(this.val() == 'Internal') {
-					$('#Form_ItemEditForm_URL_Holder').hide();
-				} else {
-					$('#Form_ItemEditForm_InternalPageID_Holder').hide();
-				}
-                this._super();
-            },
-            onchange: function(e) {
-            	if(this.val() == 'Internal') {
-					$('#Form_ItemEditForm_URL_Holder').hide();
-					$('#Form_ItemEditForm_InternalPageID_Holder').show();
-				} else {
-					$('#Form_ItemEditForm_URL_Holder').show();
-					$('#Form_ItemEditForm_InternalPageID_Holder').hide();
-				}
-            }
-        });
-    });
+	$(document).ready(function(){
+		var linkType = $('#Form_ItemEditForm_LinkType').val();
+
+		if(linkType == 'Internal') {
+			$('#Form_ItemEditForm_URL_Holder').hide();
+			$('#Form_ItemEditForm_InternalPageID_Holder').show();
+		} else {
+			$('#Form_ItemEditForm_URL_Holder').show();
+			$('#Form_ItemEditForm_InternalPageID_Holder').hide();
+		}
+
+		$('#Form_ItemEditForm_LinkType').change(function(e) {
+			var newType = $('#Form_ItemEditForm_LinkType').val();
+			if(newType == 'Internal') {
+				$('#Form_ItemEditForm_URL_Holder').hide();
+				$('#Form_ItemEditForm_InternalPageID_Holder').show();
+			} else {
+				$('#Form_ItemEditForm_URL_Holder').show();
+				$('#Form_ItemEditForm_InternalPageID_Holder').hide();			}
+		})
+	})
 })(jQuery);
